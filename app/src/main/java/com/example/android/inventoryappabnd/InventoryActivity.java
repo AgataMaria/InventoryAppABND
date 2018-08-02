6bd97a05c7a3b8666e91c330b27a2b9eaa56a907
Agata Maria <agata.karzarnowicz@gmail.com>
2018-08-02 01:01:09 +0100

    Cursor now handled by a CursorLoader and returned to the ListView using a custom adapter

--- app/src/main/java/com/example/android/inventoryappabnd/InventoryActivity.java
+++ app/src/main/java/com/example/android/inventoryappabnd/InventoryActivity.java
@@ -1,26 +1,31 @@
 package com.example.android.inventoryappabnd;
 
+import android.app.LoaderManager;
+import android.content.CursorLoader;
 import android.content.Intent;
+import android.content.Loader;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
 import android.widget.ListView;
-import android.widget.TextView;
 
 import com.example.android.inventoryappabnd.Data.InventoryContract.InventoryEntry;
 import com.example.android.inventoryappabnd.Data.InventoryCursorAdapter;
 
-public class InventoryActivity extends AppCompatActivity {
+public class InventoryActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
 
-    private String TABLE_COLUMNS_SEPARATOR = " | ";
+    // ID for the inventoryCursorLoader
+    public static final int INVENTORY_LOADER_ID = 0;
+    InventoryCursorAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inventory);
+
+        //setup a FAB to open Entry Activity, which allows to add items to the Inventory
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,24 +35,30 @@ public class InventoryActivity extends AppCompatActivity {
             }
         });
 
-        showDbInfo();
+        //setup a ListView to be populated with data from the loaded cursor
+        ListView inventoryListView = findViewById(R.id.listview);
+        adapter = new InventoryCursorAdapter(this, null);
+        inventoryListView.setAdapter(adapter);
+
+        //initiate the Loader when the Activity is created
+        getLoaderManager().initLoader(INVENTORY_LOADER_ID, null, this);
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        showDbInfo();
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        showDbInfo();
     }
 
-     private void showDbInfo() {
+    // methods required by the LoaderManager
+    @Override
+    public Loader onCreateLoader(int id, Bundle args) {
 
-        // define projection scope for the db query method
+        // define projection scope for the db operation
         String[] projection = {
                 InventoryEntry._ID,
                 InventoryEntry.COLUMN_ITEM_TYPE,
@@ -58,20 +69,34 @@ public class InventoryActivity extends AppCompatActivity {
                 InventoryEntry.COLUMN_ITEM_SUPP_NO
         };
 
-        // cursor to return db query - this one shows all info from the database
-        Cursor cursor = getContentResolver().query(InventoryEntry.CONTENT_URI, projection, null, null, null);
-
-        if (cursor != null) {
-            ListView inventoryListView = findViewById(R.id.listview);
-            InventoryCursorAdapter adapter = new InventoryCursorAdapter(this, cursor);
-            inventoryListView.setAdapter(adapter);
-        } else {
-            TextView emptyStateView = findViewById(R.id.empty_state_view_tv);
-            emptyStateView.setText(R.string.empty_state_textview_text);
+        // create a Loader with relevant ID
+        switch (id) {
+            case INVENTORY_LOADER_ID:
+                //create a CursorLoader - the cursor to return db query on all info from the database
+                return new CursorLoader(
+                        this, //context
+                        InventoryEntry.CONTENT_URI,           //queried db URI
+                        projection,    //projection for the query
+                        null,   //selection
+                        null,//selection arguments
+                        null    //sort order
+                );
+            default:
+                return null;
         }
-
     }
 
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+
+        adapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader loader) {
+        adapter.swapCursor(null);
+
+    }
 }
 
 
